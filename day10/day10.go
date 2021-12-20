e48b19112f336cdcbf9037a75e9c647dcd84ebcf
kgbuquerin <sec.gomez@protonmail.com>
2021-12-20 11:33:39 +0100

    day10 initial

---
+++ day10/day10.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func readFile(filename string) []string {
+	// read input.txt
+	file, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		fmt.Println("You need a file called input.txt in this directory!")
+	}
+	defer file.Close()
+
+	// add file content to values slice
+	sc := bufio.NewScanner(file)
+	var valuesStr []string
+
+	// convert all values in slice to integer
+	for sc.Scan() {
+		//num, _ := strconv.Atoi(sc.Text())
+		valuesStr = append(valuesStr, sc.Text())
+	}
+
+	return valuesStr
+}
+
+func firstPart() {
+	var data = readFile("input.txt")
+
+}
+
+func main() {
+	firstPart()
+	//	secondPart()
+}
